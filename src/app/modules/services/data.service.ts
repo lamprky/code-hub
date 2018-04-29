1415d9a7034a12e96fe85043dd3a0d8af6ef6bea
Michalis Anastasiou <mc.anastasiou@gmail.com>
2018-04-29 10:28:35 +0300

    Get bug list

--- src/app/modules/services/data.service.ts
+++ src/app/modules/services/data.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Bug } from '../models/bug';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class DataService {
-  private endpoint = 'api/bugs';
-
   constructor(private httpClient: HttpClient) { }
 
   getBugs(): Observable<Array<Bug>> {
-    return this.httpClient.get<Bug[]>(this.endpoint);
+    return this.httpClient.get<Bug[]>(environment.endpoint + '/bugs');
   }
 }
